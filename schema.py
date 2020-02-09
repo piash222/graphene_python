0f47bdd2ca80259d2751e0f21e8906448c8242f7
Md Shihab Uddin Piash <piash222@gmail.com>
2020-02-09 10:58:31 +0600

    Query

---
+++ schema.py
@@ -0,0 +1,32 @@
+import graphene
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
+
+class Query(graphene.ObjectType):
+    user = graphene.List(User, limit= graphene.Int())
+
+    def resolve_user(self, info, limit=None):
+        return [
+            User(id="1", username="piash", created_at= datetime.now()),
+            User(id="2", username="Tareque", created_at= datetime.now()),
+            User(id="3", username="Masum", created_at= datetime.now()),
+        ][:limit]
+
+schema = graphene.Schema(query=Query)
+result = schema.execute(
+    '''
+    {
+        user(limit:2){
+            id
+            username
+            createdAt
+        }
+    }
+    '''
+)
+
+print(result.data.items())
